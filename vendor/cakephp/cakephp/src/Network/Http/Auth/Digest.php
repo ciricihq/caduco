551d4fd6212ae9e42d5ae5d16840796e8167c13a
Enyeli <enyeli@cirici.com>
2017-01-30 13:33:31 +0100

    delete vendor folder

--- vendor/cakephp/cakephp/src/Network/Http/Auth/Digest.php
+++
@@ -1,3 +0,0 @@
-<?php
-// @deprecated Backwards compatibility with earler 3.x versions.
-class_alias('Cake\Http\Client\Auth\Digest', 'Cake\Network\Http\Auth\Digest');
--- vendor/cakephp/cakephp/src/Network/Http/Message.php
+++
@@ -1,3 +0,0 @@
-<?php
-// @deprecated Backwards compatibility with earler 3.x versions.
-class_alias('Cake\Http\Client\Message', 'Cake\Network\Http\Message');
